d8d90ad03029c07d8457c753bf845255e36a51cb
Attila Béli <thirdeye0@gmail.com>
2021-09-30 13:45:23 +0200

    react code implemented

--- src/App.js
+++ src/App.js
@@ -1,14 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [reading, setReading] = useState(null);
+  const [isConnected, setIsConnected] = useState(false);
+
+  const options = {
+    primaryService: "0000780a-0000-1000-8000-00805f9b34fb",
+    measurement: "00008aa1-0000-1000-8000-00805f9b34fb",
+    intermediateMeasurement: "00008aa2-0000-1000-8000-00805f9b34fb",
+  };
+  const filters = {
+    filters: [
+      {
+        name: "01136B",
+        services: [options.primaryService],
+      },
+    ],
+  };
+
+  function isWebBluetoothEnabled() {
+    if (navigator.bluetooth) {
+      console.log("BT enabled");
+      return true;
+    } else {
+      console.log(
+        "Web Bluetooth API is not available.\n" +
+          'Please make sure the "Experimental Web Platform features" flag is enabled.'
+      );
+      return false;
+    }
+  }
+
+  const onDisconnected = (event) => {
+    console.log(`The device ${event.target.name} is disconnected`);
+    setReading(null);
+    setIsConnected(false);
+  };
+
+  const connectToScale = () => {
+    isWebBluetoothEnabled();
+    navigator.bluetooth
+      .requestDevice(filters)
+      .then((device) => {
+        setIsConnected(true);
+        device.addEventListener("gattserverdisconnected", onDisconnected);
+        console.log("Connecting to GATT server...");
+        return device.gatt.connect();
+      })
+      .then((server) => startWeightNotifications(server));
+  };
+
+  function startWeightNotifications(server) {
+    console.log("Getting characteristics...");
+    return server.getPrimaryService(options.primaryService).then((service) => {
+      return Promise.all([
+        service
+          .getCharacteristic(options.intermediateMeasurement)
+          .then((characteristic) => handleScaleMeasurement(characteristic)),
+      ]);
+    });
+  }
+
+  // const readDescriptor = (characteristic) => {
+  //   return characteristic.getDescriptors().then((descriptor) => {
+  //     console.log(descriptor);
+  //     descriptor[0].readValue().then((value) => console.log(value));
+  //   });
+  // };
+
+  function handleScaleMeasurement(characteristic) {
+    console.log(characteristic);
+    console.log("Starting notifications for measurement...");
+    return characteristic
+      .startNotifications()
+      .then((characteristic) => {
+        console.log("Adding event listener for measurement...");
+        characteristic.addEventListener(
+          "characteristicvaluechanged",
+          onCharacteristicValueChanged
+        );
+      })
+      .catch((error) => console.error(error));
+  }
+
+  const onCharacteristicValueChanged = (event) => {
+    console.log(`READING: ${event.target.value.getUint8(0)}`);
+    setReading(event.target.value.getUint8(0));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <>
+      <center style={{ paddingTop: 50 }}>
+        <p>{isConnected ? "CONNECTED" : "NOT CONNECTED"}</p>
+        <h2>{reading ? { reading } : "No reading yet"}</h2>
+        {!isConnected && (
+          <button onClick={connectToScale}>CONNECT TO SCALE</button>
+        )}
+      </center>
+    </>
   );
-}
+};
 
 export default App;
